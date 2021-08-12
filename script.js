9a43062d9fd5286dcdad4a5012a7b81a95e756de
grftr <65460369+tevissaur@users.noreply.github.com>
2021-08-12 15:07:50 +0000

    added some comments

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-document.getElementById('start').addEventListener('click', function() {
-  if (document.getElementById('board').style.visibility === 'visible') {
-    document.getElementById('board').style.visibility = 'hidden'
-  } else {
-    document.getElementById('board').style.visibility = 'visible'
-  }
-})
+// Add event listeners
+// Draw to DOM
+// Track data
+// Computer Turn
+// Assign sides
 
-for (var i = 0; i < document.getElementsByClassName('tile').length; i++) {
-  document.getElementsByClassName('tile')[i].addEventListener('click', function(event) {
-    console.log('clicked ' + event.target.id)
-  })
-}
+
+
+
+// document.getElementById('start').addEventListener('click', function() {
+//   if (document.getElementById('board').style.visibility === 'visible') {
+//     document.getElementById('board').style.visibility = 'hidden'
+//   } else {
+//     document.getElementById('board').style.visibility = 'visible'
+//   }
+// })
+
+// for (var i = 0; i < document.getElementsByClassName('tile').length; i++) {
+//   document.getElementsByClassName('tile')[i].addEventListener('click', function(event) {
+//     console.log('clicked ' + event.target.id)
+//   })
+// }
 
