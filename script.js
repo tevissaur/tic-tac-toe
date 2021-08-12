bdc601cbc27314d47ef4a0b4d204aa82805ad403
grftr <65460369+tevissaur@users.noreply.github.com>
2021-08-12 15:03:59 +0000

    playing around in replit ide

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('start').addEventListener('click', function() {
+  if (document.getElementById('board').style.visibility === 'visible') {
+    document.getElementById('board').style.visibility = 'hidden'
+  } else {
+    document.getElementById('board').style.visibility = 'visible'
+  }
+})
+
+for (var i = 0; i < document.getElementsByClassName('tile').length; i++) {
+  document.getElementsByClassName('tile')[i].addEventListener('click', function(event) {
+    console.log('clicked ' + event.target.id)
+  })
+}
+
